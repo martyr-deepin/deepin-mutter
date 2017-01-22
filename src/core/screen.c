29ac3f84b48567f26ac133d0eee25fd8a2bcd939
Sian Cao <yinshuiboy@gmail.com>
2017-01-22 17:34:46 +0800

    core: Fix corner position calculation
    Now we take monitors topology into considration
    
    Change-Id: I3e709b7fb2a48a2be7e92b8c2082740542341b56

--- src/core/screen.c
+++ src/core/screen.c
@@ -529,31 +529,15 @@ create_guard_window (Display *xdisplay, MetaScreen *screen)
 #define CORNER_SIZE 32
 
 static Window
-create_screen_corner_window (Display *xdisplay, MetaScreen *screen, MetaScreenCorner corner)
+create_screen_corner_window (Display *xdisplay, MetaScreen *screen, MetaScreenCorner corner, int x, int y)
 {
   XSetWindowAttributes attributes;
   Window edge_window;
   gulong create_serial;
-  int x = 0, y = 0, w = CORNER_SIZE, h = CORNER_SIZE;
+  int w = CORNER_SIZE, h = CORNER_SIZE;
 
   attributes.event_mask = PointerMotionMask | EnterWindowMask | LeaveWindowMask;
   attributes.override_redirect = True;
-
-  switch (corner) {
-      case META_SCREEN_TOPLEFT:
-          x = y = 0;
-          break;
-      case META_SCREEN_TOPRIGHT:
-          x = screen->rect.width - CORNER_SIZE; y = 0; break;
-      case META_SCREEN_BOTTOMLEFT:
-          x = 0;
-          y = screen->rect.height - CORNER_SIZE; 
-          break;
-      case META_SCREEN_BOTTOMRIGHT:
-          x = screen->rect.width - CORNER_SIZE;
-          y = screen->rect.height - CORNER_SIZE;
-          break;
-  }
 
   /* We have to call record_add() after we have the new window ID,
    * so save the serial for the CreateWindow request until then */
@@ -946,21 +930,71 @@ meta_screen_create_guard_window (MetaScreen *screen)
     screen->guard_window = create_guard_window (screen->display->xdisplay, screen);
 }
 
+static void meta_screen_calc_corner_positions (MetaScreen *screen, int* positions)
+{
+    int width = screen->rect.width, height = screen->rect.height;
+
+    int n = screen->n_monitor_infos;
+    int tl_x, tl_y, tr_x, tr_y, bl_x, bl_y, br_x, br_y;
+
+    tl_x = 0;
+    tl_y = height;
+    bl_x = 0;
+    bl_y = 0;
+    for (int i = 0; i < n; i++) {
+        // test if monitor is on the left
+        MetaRectangle geo = screen->monitor_infos[i].rect;
+        if (geo.x != 0) {
+            continue;
+        }
+
+        tl_y = MIN (geo.y, tl_y);
+        bl_y = MAX (geo.y + geo.height, bl_y);
+    }
+
+
+    tr_x = width;
+    tr_y = height;
+    br_x = width;
+    br_y = 0;
+    for (int i = 0; i < n; i++) {
+        // test if monitor is on the right
+        MetaRectangle geo = screen->monitor_infos[i].rect;
+        if (geo.x + geo.width != width) {
+            continue;
+        }
+
+        tr_y = MIN (geo.y, tr_y);
+        br_y = MAX (geo.y + geo.height, br_y);
+    }
+
+    int result[] = {
+        tl_x, tl_y,
+        tr_x - CORNER_SIZE, tr_y,
+        bl_x, bl_y - CORNER_SIZE, 
+        br_x - CORNER_SIZE, br_y - CORNER_SIZE,
+    };
+
+    memcpy (positions, result, sizeof result);
+}
+
 void
 meta_screen_create_corner_window (MetaScreen *screen)
 {
-  int i;
-  MetaScreenCorner corners[] = {
-      META_SCREEN_TOPLEFT,
-      META_SCREEN_TOPRIGHT,
-      META_SCREEN_BOTTOMLEFT,
-      META_SCREEN_BOTTOMRIGHT,
-  };
+    MetaScreenCorner corners[] = {
+        META_SCREEN_TOPLEFT,
+        META_SCREEN_TOPRIGHT,
+        META_SCREEN_BOTTOMLEFT,
+        META_SCREEN_BOTTOMRIGHT,
+    };
 
-  for (i = 0; i < 4; i++) 
+    int positions[8];
+    meta_screen_calc_corner_positions (screen, positions);
+
+    for (int i = 0; i < 4; i++) 
     {
         screen->corner_windows[i] = create_screen_corner_window (
-                screen->display->xdisplay, screen, corners[i]);
+                screen->display->xdisplay, screen, corners[i], positions[i*2], positions[i*2+1]);
     }
 }
 
@@ -2592,39 +2626,16 @@ on_monitors_changed (MetaMonitorManager *manager,
 
   if (screen->corner_windows[0] != None) 
     {
-      int i;
-      MetaScreenCorner corners[] = {
-          META_SCREEN_TOPLEFT,
-          META_SCREEN_TOPRIGHT,
-          META_SCREEN_BOTTOMLEFT,
-          META_SCREEN_BOTTOMRIGHT,
-      };
+      int positions[8];
+      meta_screen_calc_corner_positions (screen, positions);
 
+      int i;
       for (i = 0; i < 4; i++) 
         {
           XWindowChanges changes;
-          int x = 0, y = 0;
 
-          switch (corners[i]) {
-              case META_SCREEN_TOPLEFT:
-                  x = y = 0;
-                  break;
-              case META_SCREEN_TOPRIGHT:
-                  x = screen->rect.width - CORNER_SIZE;
-                  y = 0;
-                  break;
-              case META_SCREEN_BOTTOMLEFT:
-                  x = 0;
-                  y = screen->rect.height - CORNER_SIZE; 
-                  break;
-              case META_SCREEN_BOTTOMRIGHT:
-                  x = screen->rect.width - CORNER_SIZE;
-                  y = screen->rect.height - CORNER_SIZE;
-                  break;
-          }
-
-          changes.x = x;
-          changes.y = y;
+          changes.x = positions[i*2];
+          changes.y = positions[i*2+1];
           changes.stack_mode = Above;
 
           XConfigureWindow(screen->display->xdisplay, screen->corner_windows[i],
