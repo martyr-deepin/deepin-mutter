655f2f80731f41f7ac7534ffcecec2e473a534eb
Sian Cao <yinshuiboy@gmail.com>
2017-03-29 11:10:42 +0800

    blur: Fix blur problem caused by locale
    Sprintf is locale sensitive and will change representation of some
    nuemrical punctuations. so will need to keep POSIX when generate GLSL
    source code.
    
    Change-Id: Ic438a6d0feddb50ed1a8d5bb75bd3d2fa7753fb9

--- src/compositor/blur-utils.c
+++ src/compositor/blur-utils.c
@@ -1,6 +1,9 @@
 #include "blur-utils.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <locale.h>
+#include <glib.h>
 
 static void build_gaussian_blur_kernel(int* pradius, float* offset, float* weight)
 {
@@ -36,45 +39,45 @@ static void build_gaussian_blur_kernel(int* pradius, float* offset, float* weigh
 
 char *build_shader(int direction, int radius, float* offsets, float *weight)
 {
-    char* ret = (char*)malloc(10000);
-    if (!ret) {
+    GString *sbuf = g_string_sized_new(4096);
+    if (!sbuf) {
         return NULL;
     }
 
-    char *p = ret;
+    char *orig_lc_num = g_strdup(setlocale(LC_NUMERIC, NULL));
+
+    setlocale(LC_NUMERIC, "POSIX");
 
     if (direction == VERTICAL) {
-        int sz = sprintf(p, "vec2 tc = cogl_tex_coord.st;\n"
+        g_string_append_printf(sbuf, "vec2 tc = cogl_tex_coord.st;\n"
                 "cogl_texel = texture2D(cogl_sampler, tc) * %f;\n",
                 weight[0]);
-        p += sz;
 
         for (int i = 1; i < radius; i++) {
-            sz = sprintf(p, 
+            g_string_append_printf(sbuf, 
                     "cogl_texel += texture2D(cogl_sampler, tc - vec2(0.0, %f/resolution.y)) * %f; \n"
                     "cogl_texel += texture2D(cogl_sampler, tc + vec2(0.0, %f/resolution.y)) * %f; \n",
                     offsets[i], weight[i],
                     offsets[i], weight[i]);
-            p += sz;
         }
     } else {
 
-        int sz = sprintf(p,
+        g_string_append_printf(sbuf,
                 "vec2 tc = vec2(cogl_tex_coord.s, 1.0 - cogl_tex_coord.t); \n"
                 "cogl_texel = texture2D(cogl_sampler, tc) * %f;\n",
                 weight[0]);
-        p += sz;
 
         for (int i = 1; i < radius; i++) {
-            sz = sprintf(p, 
+            g_string_append_printf(sbuf, 
                     "cogl_texel += texture2D(cogl_sampler, tc - vec2(%f/resolution.x, 0.0)) * %f; \n"
                     "cogl_texel += texture2D(cogl_sampler, tc + vec2(%f/resolution.x, 0.0)) * %f; \n",
                     offsets[i], weight[i],
                     offsets[i], weight[i]);
-            p += sz;
         }
     }
     
-    return ret;
+    setlocale(LC_NUMERIC, orig_lc_num);
+    g_clear_pointer (&orig_lc_num, g_free);
+    return g_string_free(sbuf, FALSE);
 }
 
